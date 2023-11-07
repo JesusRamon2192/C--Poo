b9ec20ccead3b1400bb95f183bf98515bdcdf5ce
jesus <jesus.ramon2192@gmail.com>
2023-11-06 21:16:20 -0600

    First

---
+++ Models/SuperHeroe.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroesApp.Models
+{
+
+    class SuperHeroe
+    {
+        private string _Nombre;
+        public int Id;
+        public string Nombre
+        {
+            get 
+            {
+                return _Nombre;
+            }
+
+            set
+            {
+                _Nombre = value.Trim();
+            }
+        }
+
+        public string NombreEIdentidadSecreta
+        {
+            get
+            {
+                return $"{Nombre}({IdentidadSecreta})";
+            }
+        }
+
+        public string IdentidadSecreta;
+        public string Ciudad;
+        public List<SuperPoder> SuperPoderes;
+        public bool PuedeVolar;
+
+        public SuperHeroe()
+        {
+            Id = 1;
+            SuperPoderes = new List<SuperPoder>();
+            PuedeVolar = false;
+        }
+
+        public string UsarSuperPoderes()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in SuperPoderes)
+            {
+                sb.AppendLine($"{NombreEIdentidadSecreta} está usando el super poder {item.Nombre}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SuperHeroesApp.Models;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "SuperFuerza";
+superFuerza.Nivel = NivelPoder.NivelTres;
+
+var superman = new SuperHeroe();
+superman.Id = 1;
+superman.Nombre = "      Superman     ";
+superman.IdentidadSecreta = "Clark";
+superman.Ciudad = "Metropolis";
+superman.PuedeVolar = true;
+
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(poderVolar);
+poderesSuperman.Add(superFuerza);
+superman.SuperPoderes = poderesSuperman;
+string resultSuperPoderes = superman.UsarSuperPoderes();
+Console.WriteLine(resultSuperPoderes); 
+
+var spiderman = new SuperHeroe();
+spiderman.Id = 2;
+spiderman.Nombre = "Spiderman";
+spiderman.IdentidadSecreta = "Peter"; // Corregido el nombre
+spiderman.Ciudad = "New York";
+spiderman.PuedeVolar = false;
+
+List<SuperPoder> poderesSpiderman = new List<SuperPoder>();
+poderesSpiderman.Add(superFuerza);
+spiderman.SuperPoderes = poderesSpiderman;
+
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTres
+}
